4db36b49ce672f7bfa5f288d38ff89dc4b4caa2f
Firmiana Wang <firmianaw@gmail.com>
2020-07-15 21:52:54 -0500

    Scrollview added to each category scene. GirlsOnlyIndividualViewController is a swift file for the Girls Only individual scene where the labels will change.

---
+++ STEAMtunity/GirlsOnlyIndividualViewController.swift
@@ -0,0 +1,28 @@
+//
+//  GirlsOnlyIndividualViewController.swift
+//  STEAMtunity
+//
+//  Created by Firmiana on 7/15/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class GirlsOnlyIndividualViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var linkLabel: UILabel!
+    @IBOutlet weak var whoLabel: UILabel!
+    @IBOutlet weak var whatLabel: UILabel!
+    @IBOutlet weak var whereLabel: UILabel!
+    @IBOutlet weak var whenLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
